a4bac887471e28d630a99d454830af489c3acbba
Mikhail <mixartemev@gmail.com>
2017-10-15 20:19:55 +0300

    Update params-local.php

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -5,7 +5,7 @@ return [
     'frontendHostInfo' => 'http://shop.dev',
     'backendHostInfo' => 'http://backend.shop.dev',
     'staticHostInfo' => 'http://static.shop.dev',
-    'mailChimpKey' => '4bfabc4d46198894e817ec7defacdfeb-us12',
+    'mailChimpKey' => '',
     'mailChimpListId' => '',
     'smsRuKey' => '',
 ];
